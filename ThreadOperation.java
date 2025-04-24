d54cbb6940758217e48853ac5ece6564735752fe
Fielding <fieldingparsons@gmail.com>
2025-04-24 02:29:04 -0600

    filled in constructor

--- ThreadOperation.java
+++ ThreadOperation.java
@@ -6,13 +6,16 @@ Matrix addition
 CSCI 2251 R01
 */
 
+
 public class ThreadOperation extends Thread {
 	private int[][] submatrixOfA;
 	private int[][] submatrixOfB;
 	private enum Quadrant {UPPER_LEFT, UPPER_RIGHT, LOWER_LEFT, LOWER_RIGHT};
-	
+	Quadrant quadrant;
 	public ThreadOperation(int[][] submatrixOfA, int[][] submatrixOfB, Quadrant quadrant) { //https://www.geeksforgeeks.org/enum-in-java/
-		
+		this.submatrixOfA = submatrixOfA;
+		this.submatrixOfB = submatrixOfB;
+		this.quadrant = quadrant;
 	}
 	@Override //not sure if i need this, double check
 	public void run() {
