ae85460c23898e7ae5fc02b85e949b2babd9a981
Fielding <fieldingparsons@gmail.com>
2025-04-24 02:13:01 -0600

    attempted to start framework of constructor

--- ThreadOperation.java
+++ ThreadOperation.java
@@ -11,6 +11,9 @@ public class ThreadOperation extends Thread {
 	private int[][] submatrixOfB;
 	private enum Quadrant {UPPER_LEFT, UPPER_RIGHT, LOWER_LEFT, LOWER_RIGHT};
 	
+	public ThreadOperation(int[][] submatrixOfA, int[][] submatrixOfB, Quadrant) { // for some reason wont let me put enum in here, but doesn't recognize quadrant; research needed
+		
+	}
 	@Override //not sure if i need this, double check
 	public void run() {
 		return;
