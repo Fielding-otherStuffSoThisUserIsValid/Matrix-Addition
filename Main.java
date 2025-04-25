aa22d514d99a1919809ebf71e5dabdb6ca88b207
Fielding <fieldingparsons@gmail.com>
2025-04-24 18:34:48 -0600

    started on instantiating ThreadOperations
    stopped to make some really basic arrays just so i could use the constructors properly

--- Main.java
+++ Main.java
@@ -32,9 +32,8 @@ public class Main
 {
 	public static void main(String[] args) 
 	{
-		
 		System.out.println();
-		
+		//ThreadOperation UpperLeftThreadOperation = new ThreadOperation()
 		
 		
 	}
