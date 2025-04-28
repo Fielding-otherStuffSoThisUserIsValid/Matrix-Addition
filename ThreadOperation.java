70251b43626393d254be306f65f7bdbc02f3abc8
Fielding <fieldingparsons@gmail.com>
2025-04-28 00:32:35 -0600

    cleaned up ThreadOperation a little

--- ThreadOperation.java
+++ ThreadOperation.java
@@ -10,13 +10,14 @@ CSCI 2251 R01
 public class ThreadOperation extends Thread {
 	private int[][] submatrixOfA;
 	private int[][] submatrixOfB;
-	
 	Quadrant quadrant;
+	
 	public ThreadOperation(int[][] submatrixOfA, int[][] submatrixOfB, Quadrant quadrant) { //https://www.geeksforgeeks.org/enum-in-java/
 		this.submatrixOfA = submatrixOfA;
 		this.submatrixOfB = submatrixOfB;
 		this.quadrant = quadrant;
 	}
+	
 	@Override //not sure if i need this, double check
 	public void run() {
 		return;
